e0c12dd8f88ccbcc28864ab132a102931abe6efe
Michael Yeboah <mbaah80@gmail.com>
2023-01-27 20:16:48 +0000

    jobs

--- src/components/jobs/jobs.js
+++ src/components/jobs/jobs.js
@@ -11,42 +11,34 @@ let Jobs = () => {
         <div className="container">
             <div className="row">
                 <div className="col-md-3">
-                    <div className="card">
+                    <div className="card leftCard">
                         <div className="card-body">
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-users mr-2 ml-2" aria-hidden="true"></i> Connections</p>
-                                <p className="actionNumbers">19</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-bookmark" aria-hidden="true"></i> My jobs</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-address-book mr-2 ml-2" aria-hidden="true"></i>Contacts</p>
-                                <p className="actionNumbers">199</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-bell" aria-hidden="true"></i> Jobs alert</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-user mr-2 ml-2" aria-hidden="true"></i> Following & followers</p>
-                                <p className="actionNumbers">9</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-check" aria-hidden="true"></i> Demonstrate skills</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-users mr-2 ml-2" aria-hidden="true"></i>Groups</p>
-                                <p className="actionNumbers">19</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-file-text" aria-hidden="true"></i> Interview prep</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-calendar-check-o mr-2 ml-2" aria-hidden="true"></i> Events</p>
-                                <p className="actionNumbers">9</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-file" aria-hidden="true"></i> Resume Builder</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-building-o mr-2 ml-2" aria-hidden="true"></i>Pages</p>
-                                <p className="actionNumbers">19</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-bell" aria-hidden="true"></i> Job seeker guidance</p>
                             </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-newspaper-o mr-2 ml-2" aria-hidden="true"></i> Newsletter</p>
-                                <p className="actionNumbers">1</p>
-                            </a>
-                            <a href="#" className="manageNetworks btn btn-default btn-sm recommedBtn"><p><i
-                                className="fa fa-hashtag mr-2 ml-2" aria-hidden="true"></i>Hashtags</p>
-                                <p className="actionNumbers">19</p>
+                            <a href="#" className=" btn btn-default btn-sm ">
+                                <p><i className="fa fa-cog" aria-hidden="true"></i> Application settings</p>
                             </a>
                         </div>
                     </div>
+                    <button className="btn btn-primary postJobBtn">
+                        <i className="fa fa-pencil-square-o" aria-hidden="true"></i> Post a free job
+                    </button>
                 </div>
                 <div className="col-md-5 middleCard">
                     <div className="card  mb-3">
